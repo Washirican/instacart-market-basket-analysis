95a03761806ec3c181ba9d3b7f5931a40ef17d41
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2025-08-21 15:53:01 -0700

    Add TODOs.

--- s3_test.py
+++ s3_test.py
@@ -1,12 +1,16 @@
 # Create a SparkSession
 from pyspark.sql import SparkSession
 
+
 def main():
+    # TODO (2025-08-21): Fix to use JAR files
+    # TODO (2025-08-21): Fix to use credentials from environment variables
     spark = SparkSession.builder \
         .appName("ReadFromS3") \
+        .config("spark.jars.packages","org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
         .config("spark.hadoop.fs.s3a.aws.credentials.provider",
                 "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
         .getOrCreate()
 
     aisles_df = spark.read.csv('s3a://washirican-aws-bucket/dataset/aisles.csv', inferSchema=True)
@@ -16,6 +20,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # Run with this: spark-submit \
-    # 		     --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
-    #                s3_test.py
+    # Run with this:
+    # spark-submit --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 s3_test.py
